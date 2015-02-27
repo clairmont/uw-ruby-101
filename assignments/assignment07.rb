f303ab61e150d179f7db9be46322fcce688d8c0b
clairmont <clairmont@gmail.com>
2015-02-26 17:46:22 -0800

    HW7 for AlexC
    Assertion isn't able to call the private method; I was hoping public would let it work but it wasn't..

--- assignments/assignment07.rb
+++ assignments/assignment07.rb
@@ -8,25 +8,67 @@
 # implement Observable
 # validate using MiniTest unit tests
 
-module Assignment07
-  module Observable
+  require 'MiniTest/autorun'
+  module Assignment07
+    module Observable
+    def initial_observers
+      @observers = Array.new
+    end
     def add_observer(obj)
-      # your implementation here
+      @observers << obj
     end
     def delete_observer(obj)
-      # your implementation here
+      @observers.delete obj
     end
     def delete_observers
-      # your implementation here
+      @observers = Array.new
     end
+
+    def count
+        @observers.count
+    end
+
     def changed(new_state=true)
-      # your implementation here
+      @status = true
     end
     def changed?
-      # your implementation here
+      @status == true
     end
     def notify_observers(*args)
-      # your implementation here
+      if @status
+      @observers.each {|x| x.update(*args)}
+      @status = false
+      end
     end
   end
-end
+
+    class Observed
+     include Assignment07::Observable
+    end
+    class Observer
+      def initialize
+       @headcount = 1
+      end
+      def headcount
+        @headcount
+      end
+    end
+
+
+  class TestObserver < Minitest::Unit::TestCase
+
+    def setup
+    @content = Observed.new
+    @viewer1 = Observer.new
+    @viewer2 = Observer.new
+    @viewer3 = Observer.new
+    end
+
+    def test01
+    @content.add_observer(@viewer1)
+    @content.add_observer(@viewer2)
+    @content.add_observer(@viewer3)
+
+    assert_equal 3, @content.count
+    end
+  end
